0e18b7e49c3748bfbe001a29df754ed1b5c510b6
mgecer <melike.gecer@unil.ch>
2020-10-07 10:08:01 +0200

    solutions of w4 and typo fix

--- 2020/ShoppingWebsite_v1/src/test/java/ch/unil/doplab/shoppingwebsite/controllers/ItemControllerTest.java
+++ 2020/ShoppingWebsite_v1/src/test/java/ch/unil/doplab/shoppingwebsite/controllers/ItemControllerTest.java
@@ -4,12 +4,13 @@ import ch.unil.doplab.shoppingwebsite.UserList;
 import ch.unil.doplab.shoppingwebsite.exceptions.AlreadyExistsException;
 import ch.unil.doplab.shoppingwebsite.exceptions.DoesNotExistException;
 import ch.unil.doplab.shoppingwebsite.items.Drink;
+import ch.unil.doplab.shoppingwebsite.items.Food;
 import ch.unil.doplab.shoppingwebsite.users.Admin;
 import ch.unil.doplab.shoppingwebsite.users.Buyer;
 import ch.unil.doplab.shoppingwebsite.users.Seller;
 import ch.unil.doplab.shoppingwebsite.users.User;
+import java.util.ArrayList;
 import org.testng.Assert;
-import static org.testng.Assert.fail;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -156,43 +157,117 @@ public class ItemControllerTest {
     public void testAddAFoodToShop() throws DoesNotExistException, AlreadyExistsException {
         System.out.println("@Test testAddAFoodToShop");
         // login as a seller
+        Seller seller = (Seller) UserController.userLogsIn("Barty's", "1234");
         // we need some mock data
+        String foodName = "Souffle";
+        double foodPrice = 11.0;
+        boolean containsMeat = false;
         // add it to the shop
+        ItemController.addAFoodToShop(seller, foodName, foodPrice, containsMeat, new ArrayList() {
+            {
+                add("flour");
+                add("coco powder");
+                add("milk");
+                add("egg");
+                add("sugar");
+            }
+        });
         // find the food in the shop
-        // if the food exists, expectedFood must have a value
-        fail("TODO: Complete this test"); // dont forget to remove this line when you complete the method
+        Food expecedFood = null;
+        for (Food f : seller.getFoods()) {
+            if (f.getName().equals(foodName)) {
+                expecedFood = f;
+            }
+        }
+        // if the food exists, expecedFood must have a value
+        Assert.assertNotNull(expecedFood);
     }
 
     @Test
     public void testRemoveAFoodFromShop() throws DoesNotExistException {
         System.out.println("@Test testRemoveAFoodFromShop");
         // login as a seller
+        Seller seller = (Seller) UserController.userLogsIn("Barty's", "1234");
         // we need some mock data
+        String foodName = "Souffle";
         // remove the food
+        ItemController.removeAFoodFromShop(seller, foodName);
         // it should not exist in the shop
+        Food expectedFood = null;
+        for (Food f : seller.getFoods()) {
+            if (f.getName().equals(foodName)) {
+                expectedFood = f;
+            }
+        }
         // if the food does not exist, expectedFood must be null
-        fail("TODO: Complete this test"); // dont forget to remove this line when you complete the method
+        Assert.assertNull(expectedFood);
     }
 
     @Test
     public void testAddAFoodToShoppingCart() throws DoesNotExistException {
         System.out.println("@Test testAddAFoodToShoppingCart");
         // login as a buyer
+        Buyer buyer = (Buyer) UserController.userLogsIn("marge", "1234");
         // we need some mock data
+        String shopName = "Barty's";
+        String foodName = "Souffle";
         // add the food to the shopping cart
+        ItemController.addAFoodToShoppingCart(buyer, shopName, foodName);
         // find the food in the shopping cart
+        Food expectedFood = null;
+        for (Food f : buyer.getShoppingCart().getFoods()) {
+            if (f.getName().equals(foodName)) {
+                expectedFood = f;
+            }
+        }
         // if the food exists, expectedFood must have a value
-        fail("TODO: Complete this test"); // dont forget to remove this line when you complete the method
+        Assert.assertNotNull(expectedFood);
     }
 
     @Test
     public void testRemoveAFoodFromShoppingCart() throws DoesNotExistException {
         System.out.println("@Test testRemoveAFoodFromShoppingCart");
         // login as a buyer
+        Buyer buyer = (Buyer) UserController.userLogsIn("marge", "1234");
         // we need some mock data
+        String foodName = "Souffle";
         // remove the food
+        ItemController.removeAFoodFromShoppingCart(buyer, foodName);
         // it should not exist in the shopping cart
+        Food expectedFood = null;
+        for (Food f : buyer.getShoppingCart().getFoods()) {
+            if (expectedFood.getName().equals(foodName)) {
+                expectedFood = f;
+            }
+        }
         // if the food does not exist, expectedFood must be null
-        fail("TODO: Complete this test"); // dont forget to remove this line when you complete the method
+        Assert.assertNull(expectedFood);
+    }
+
+    // this test fails
+    @Test(timeOut = 10)
+    public void testFailingTimeOut() throws InterruptedException {
+        System.out.println("@Test failTimeOut");
+        Thread.sleep(500);
+    }
+
+    @Test(expectedExceptions = NumberFormatException.class)
+    public void testNumberFormatException() {
+        System.out.println("@Test testNumberFormatException");
+        int n = Integer.parseInt(null);
+    }
+
+    @Test(expectedExceptions = ClassCastException.class)
+    public void testClassCastException() {
+        System.out.println("@Test testClassCastException");
+        Object o = Integer.valueOf(42);
+        String s = (String) o;
+
+    }
+
+    @Test(expectedExceptions = ArithmeticException.class)
+    public void testArithmeticException() {
+        System.out.println("@Test testArithmeticException");
+        int result = 7 / 0;
     }
 }
